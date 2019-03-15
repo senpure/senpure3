d4405a8e5e00606abaa6c53b076a60b7f44081f2
senpure <senpure@senpure.com>
2019-03-15 15:47:00 +0800

    normal

--- senpure-snowflake-dispatcher/src/main/java/com/senpure/snowflake/configuration/DatabaseConfiguration.java
+++ senpure-snowflake-dispatcher/src/main/java/com/senpure/snowflake/configuration/DatabaseConfiguration.java
@@ -9,8 +9,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
@@ -21,26 +19,28 @@ import javax.sql.DataSource;
  * @time 2019-01-04 10:58:29
  */
 @Configuration
-@EnableTransactionManagement
-@EnableJpaRepositories
+//@EnableTransactionManagement
+//@EnableJpaRepositories
 public class DatabaseConfiguration {
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Bean(name = "dataSourceProperties")
-    @ConfigurationProperties(prefix = "spring.datasource")
+    @ConfigurationProperties(prefix = "spring.datasource.druid")
     @Primary
     public DataSourceProperties dataSourceProperties() {
         return new DataSourceProperties();
     }
 
 
-    @Bean(name = "dataSource")
-    @ConfigurationProperties(prefix = "spring.datasource")
+    //@Bean(name = "dataSource")
+   // @ConfigurationProperties(prefix = "spring.datasource")
     @Primary
+    @Bean
     public DataSource dataSource() {
         DataSourceProperties prop = dataSourceProperties();
         DatabaseUtil.checkAndCreateDatabase(prop);
         return DruidDataSourceBuilder.create().build();
+
     }
 
 
